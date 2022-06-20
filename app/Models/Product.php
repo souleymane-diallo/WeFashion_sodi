8720d500cb3c20c989b2e758f969520c5a505634
souley224 <diallosouleymanekolon@gmail.com>
2022-06-20 15:34:03 +0200

    add modele relationship with tables

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'reference',
+        'visibility',
+        'state',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function picture()
+    {
+        return $this->hasOne(Picture::class);
+    }
+
+    public function sizes()
+    {
+        return $this->belongsToMany(Size::class);
+    }
+}
