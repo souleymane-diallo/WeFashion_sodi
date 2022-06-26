951d01ef4fc126a3998c31e4dd29a6cf58bf7be3
souley224 <diallosouleymanekolon@gmail.com>
2022-06-26 16:36:57 +0200

    fix add commentaire

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Category;
 use App\Models\Product;
 
 class FrontController extends Controller
@@ -18,31 +16,34 @@ class FrontController extends Controller
         return view('front.index', compact('products'));
     }
 
+    // display a product in detail from its id
     public function show(int $id)
     {
         $product = Product::with('category', 'picture')->find($id);
         return view('front.show', compact('product'));
     }
 
+    // display products on sale
     public function showProductBySale()
     {
-        $products = Product::published()->with('picture')->where('state', 'sale')->paginate($this->paginate);
-        // dd($products);
-        // $sales = Product::where('state', 'sale')->get();
+        $products = Product::published()->with('category','picture')->where('state', 'sale')->paginate($this->paginate);
+
         return view('front.sale', compact('products'));
     }
 
+    // display products that have category men
     public function showProductByMan()
     {
         $products = Product::published()->with('category', 'picture')->where('category_id', 1)->paginate($this->paginate);
-        // dd($products);
+
         return view('front.man', compact('products'));
     }
 
+    // display products that have category women
     public function showProductByWoman()
     {
         $products = Product::published()->with('category', 'picture')->where('category_id', 2)->paginate($this->paginate);
-        // dd($products);
+
         return view('front.woman', compact('products'));
     }
 }
